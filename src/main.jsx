b981975b4ca3b3a52cc37f4355c5d8e65c50af90
14jatinkumar09-lang <14jatinkumar09@gmail.com>
2025-11-10 11:01:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,13 +13,13 @@ import { RecoilRoot } from "recoil";
 
 
 createRoot(document.getElementById('root')).render(
-  
+  <React.StrictMode>
 <RecoilRoot >
     <BrowserRouter>
     <App />
     </BrowserRouter>
 </RecoilRoot>
-
+</React.StrictMode>
 
  
  
