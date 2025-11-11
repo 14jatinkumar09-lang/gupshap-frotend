4577235cee2527f1d63f792afbfcd819a4408f71
14jatinkumar09-lang <14jatinkumar09@gmail.com>
2025-11-11 20:44:14 +0530

    Update UserCard.jsx

--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -8,15 +8,15 @@ export function UserCard ({onClick , user}) {
     onClick={onClick} >
             <div>
             <div className="flex gap-8 items-center w-full p-1  border border-white/10 rounded-2xl my-2">
-                <div className={`avatar ${onlineUsers?.includes(user._id)?"avatar-online" :"avatar-offline"}`}>
+                <div className={`avatar ${onlineUsers?.includes(user?._id)?"avatar-online" :"avatar-offline"}`}>
   <div className="w-15 rounded-full">
-    <img src={user.avatar} />
+    <img src={user?.avatar} />
   </div>
 </div>
 
             <div className="flex flex-col items-start">
-                <h1 className="text-xl">{user.fullName}</h1>
-                <div className="text-[10px] ">{user.userName}</div>
+                <h1 className="text-xl">{user?.fullName}</h1>
+                <div className="text-[10px] ">{user?.userName}</div>
                 </div>
             </div>
                 
